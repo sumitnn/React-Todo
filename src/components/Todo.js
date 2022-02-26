aaa7c86425fc5ec6d7183b6e96ff9c20aaf5bc69
sumitnn <sumitnautiyal446@gmail.com>
2022-02-26 22:51:40 +0530

    todo delete implementation

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-const  Todo=()=> {
+const Todo = () => {
+    const [inputvalue, setInputvalue] = useState('');
+    const [data, setData] = useState([]);
+    const deleteitem = (id) => {
+        const res = data.filter((cur,i) => {
+            return id !==i 
+        })
+        setData(res);
+    }
+    // function adddata 
+    const AddFun = () => {
+        setData([...data,inputvalue]);
+        setInputvalue("");
+
+    }
+
   return (
       <div className="container p-5">
-      
-          <div id="myDIV" className="header mt-5">
-              <h2 style={{ "margin": "5px" }}>To-Do List App</h2>
-              <input type="text" id="myInput" placeholder="Add Todo..." /> <span className="addBtn">Add</span>
+          <div id="myDIV" className="header mt-5 ">
+              <h3 style={{ "margin": "5px" }}>TODO-LIST: {data.length}</h3>
+              <input type="text" id="myInput" value={inputvalue} onChange={(e) => setInputvalue(e.target.value)} placeholder="Add Todo..." />
+              <span className="addBtn" onClick={()=>AddFun()}>Add</span>
           </div>
           <ul id="myUL">
-              <li>hi <i className="fa-solid fa-xmark" style={{"float":"right"}}></i></li>
+              {
+                  data.map((val,i) => {
+                      return (
+                          <li key={i}>{val}<i className="fa-solid fa-xmark cross p-2" onClick={()=>deleteitem(i)} style={{ "float":"right" }}></i></li>
+                      )
+                  })
+              }
+              
           </ul>
       
       </div>
